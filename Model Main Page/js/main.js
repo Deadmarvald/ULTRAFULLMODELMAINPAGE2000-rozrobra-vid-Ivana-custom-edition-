c866cdd64e3f95fed780347cdff3fe9d71abcb8b
Deadmarvald <90569835+Deadmarvald@users.noreply.github.com>
2022-06-14 15:41:01 +0300

    адаптивчікс

--- Model Main Page/js/main.js
+++ Model Main Page/js/main.js
@@ -13,3 +13,31 @@ switchMode.onclick = function () {
 		theme.href = "css/light-mode.css";
 	}
 }
+
+function outputUpdate(speed) {
+	var output = document.querySelector("#speed");
+	output.value = speed;
+	output.style.left = speed * 3.6 - 17 + 'px';
+	if(output.value > 9) {
+		output.style.left = speed * 3.6 - 25 + 'px';}
+	if(output.value > 18) {
+		output.style.left = speed * 3.6 - 26.5 + 'px';}
+	if(output.value > 27) {
+		output.style.left = speed * 3.6 - 28 + 'px';}
+	if(output.value > 36) {
+		output.style.left = speed * 3.6 - 29.5 + 'px';}
+	if(output.value > 45) {
+		output.style.left = speed * 3.6 - 31 + 'px';}
+	if(output.value > 54) {
+		output.style.left = speed * 3.6 - 32.5 + 'px';}
+	if(output.value > 63) {
+		output.style.left = speed * 3.6 - 34 + 'px';}
+	if(output.value > 72) {
+		output.style.left = speed * 3.6 - 35.5 + 'px';}
+	if(output.value > 81) {
+		output.style.left = speed * 3.6 - 37 + 'px';}
+	if(output.value > 90) {
+		output.style.left = speed * 3.6 - 38.5 + 'px';}
+	if(output.value > 99) {
+		output.style.left = speed * 3.6 - 47 + 'px';}
+}
